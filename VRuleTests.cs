82be05f8874e1cf76d40fd6169a977215feafa1c
Gatis Avots <gatis.avots@posti.com>
2021-11-07 15:21:45 +0200

    Refactored rule defining method name

--- VRuleTests.cs
+++ VRuleTests.cs
@@ -8,28 +8,28 @@ namespace Tdd2Validator
         [Fact]
         public void ValidationRuleCanBeSpecified()
         {
-            var rule = VRule<int>.Define((x) => x == 0);
+            var rule = VRule<int>.For((x) => x == 0);
         }
 
         [Fact]
         public void ValidationRuleCanBeExecuted()
         {
-            var rule = VRule<int>.Define((x) => x == 0);
+            var rule = VRule<int>.For((x) => x == 0);
             var result = rule.Validate(1);
         }
 
         [Fact]
         public void ValidCaseCanBeVerified()
         {
-            var rule = VRule<int>.Define((x) => x == 0);
+            var rule = VRule<int>.For((x) => x == 0);
             var result = rule.Validate(0);
             Assert.True(result);
         }
-        
+
         [Fact]
         public void InvalidCaseCanBeVerified()
         {
-            var rule = VRule<int>.Define((x) => x == 0);
+            var rule = VRule<int>.For((x) => x == 0);
             var result = rule.Validate(1);
             Assert.False(result);
         }
@@ -44,7 +44,7 @@ namespace Tdd2Validator
             _rule = rule;
         }
 
-        public static VRule<T> Define(Func<T, bool> rule)
+        public static VRule<T> For(Func<T, bool> rule)
         {
             return new VRule<T>(rule);
         }
